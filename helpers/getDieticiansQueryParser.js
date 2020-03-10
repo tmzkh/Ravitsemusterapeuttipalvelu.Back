55408937db38aa96f6d2a33480a355577534c084
Tommi Hyvärinen <Tommi.Hyvarinen@edu.savonia.fi>
2020-03-10 19:31:50 +0200

    fixed dietician validation, modified dietician route, added query params parser helper

---
+++ helpers/getDieticiansQueryParser.js
@@ -0,0 +1,30 @@
+const GetDieticiansQueryParser = (query) => {
+    try {
+        const expertiseIds = 
+        query.expertises
+        ? JSON.parse(query.expertises)
+        : [];
+
+        const searchQuery = 
+            query.query
+            ? query.query
+            : "";
+
+        if (typeof searchQuery != 'string')
+            return { error: "Search query must be a string" };
+
+        if (typeof expertiseIds != 'object')
+            return { error: "Expertises must be an array of integers" };
+
+        expertiseIds.forEach(id => {
+            if (isNaN(id)) 
+                return { error: "Expertises must be an array of integers" };
+        });
+
+        return { searchQuery, expertiseIds };
+    } catch {
+        return { error: "Invalid query params" };
+    }
+}
+
+module.exports = GetDieticiansQueryParser;

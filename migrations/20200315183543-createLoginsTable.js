e04f0f66534f398c0a347e12fc22375915ee4b80
Tommi Hyvärinen <Tommi.Hyvarinen@edu.savonia.fi>
2020-03-15 21:23:43 +0200

    modifications

--- migrations/20200315183543-createLoginsTable.js
+++ migrations/20200315183543-createLoginsTable.js
@@ -3,51 +3,36 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable(
-      'bookings', { 
+      'logins', { 
         id: {
           type: Sequelize.UUID,
           primaryKey: true,
           defaultValue: Sequelize.UUIDV4,
-        },
-        customerId: {
-            type: Sequelize.UUID,
-            allowNull: false,
-            references: {
-              model: 'customers',
-              key:'id'
-            }
-        },
-        dieticianId: {
-            type: Sequelize.UUID,
-            allowNull: false,
-            references: {
-              model: 'dieticians',
-              key:'id'
-            }
-        },
-        startsAt: {
-            type: Sequelize.DATE,
-            allowNull: false
-        },
-        endsAt: {
-            type: Sequelize.DATE,
-            allowNull: false
-        },
-        description: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: "Description is required"
-                },
-            }
-        },
-        createdAt: Sequelize.DATE,
-        updatedAt: Sequelize.DATE, 
+          validate: {
+              isUUID: 4,
+          }
+      },
+      userId: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          references: {
+            model: 'user',
+            key:'id'
+          }
+      },
+      accessToken: {
+          type: Sequelize.STRING,
+          allowNull: false,
+      },
+      role: {
+          type: Sequelize.STRING,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
       });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('bookings');
+    return queryInterface.dropTable('logins');
   }
 };

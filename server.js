9d2b48c7f8b4d22f67409a5c9a6a92bba0792fad
Tommi Hyvärinen <Tommi.Hyvarinen@edu.savonia.fi>
2020-04-07 17:25:23 +0300

    added accepted header

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const HOSTNAME = process.env.HOST || 'localhost';
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, AccessToken');
     next();
 });
 
